cca0d6ce907eb39775bc4ba9acab7d447f6591e4
Abhimanyu Saharan <abhi16_93@yahoo.in>
2015-02-08 11:41:53 +0530

    initial commit

---
+++ Bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Abhimanyu
+	 * Date: 08-02-2015
+	 * Time: 11:38
+	 */
+	namespace abhimanyu\installer;
+
+	use yii\base\Application;
+	use yii\base\BootstrapInterface;
+
+	class Bootstrap implements BootstrapInterface
+	{
+		/**
+		 * Bootstrap method to be called during application bootstrap stage.
+		 *
+		 * @param Application $app the application currently running
+		 */
+		public function bootstrap($app)
+		{
+			// TODO: Implement bootstrap() method.
+		}
+	}
---
+++ Module.php
@@ -0,0 +1,13 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Abhimanyu
+	 * Date: 08-02-2015
+	 * Time: 11:29
+	 */
+	namespace abhimanyu\installer;
+
+	class Module extends \yii\base\Module
+	{
+
+	}
